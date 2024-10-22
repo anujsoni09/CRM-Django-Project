dba62af249206846f0fe1cc4ffb166037e6ee7b6
Anuj <20bcs3023@gmail.com>
2024-10-23 01:47:39 +0530

    django-admin

--- leads/models.py
+++ leads/models.py
@@ -11,12 +11,12 @@ class Lead(models.Model):
     last_name=models.CharField(max_length=30)
     age=models.IntegerField(default=0)
     agent=models.ForeignKey("Agent",on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Agent(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    first_name=models.CharField(max_length=40)
-    last_name=models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.user.email
   
-
-
-
