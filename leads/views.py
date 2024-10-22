046e5a9c6f04cd7d77f9ca4b59e8bd16d63e4d62
Anuj <20bcs3023@gmail.com>
2024-10-23 02:22:16 +0530

    url_namespace

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lead
 
 # Create your views here.
+def home_page(request):
+    leads=Lead.objects.all()
+    context={
+        "leads":leads
+    }
+    # return HttpResponse("hello Anuj")
+    return render(request,"leads/home_page.html",context)
+
